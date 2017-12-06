4a94e89cb6febb2368c7017e06ca1bbd32f3deb5
Md Masum Billah <masumbillah@Mds-MacBook-Pro.fritz.box>
2017-12-06 22:41:08 +0100

    Suggestion router

--- web.py
+++ web.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import json
 
 from questions import Questions
 
@@ -13,3 +14,10 @@ def index():
     query = request.args.get("q", " ")
     query_result = Questions().search(query) if query else {}
     return render_template("index.html", query=query, query_result=query_result)
+
+
+@app.route("/suggestion/<query>")
+def suggestion(query):
+    data = ['Audi', 'BMW', 'Bugatti', 'Ferrari', 'Ford', 'Lamborghini', 'Mercedes Benz', 'Porsche', 'Rolls-Royce',
+            'Volkswagen']
+    return json.dumps(data), {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
