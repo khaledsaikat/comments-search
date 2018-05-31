b9dcb6af40ce66c4e5a2ee5712c18a451bb44920
Khaled <khaledsaikat@gmail.com>
2018-05-31 17:56:39 +0200

    implement mean of precision recall & f1

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,13 @@
-from typing import List
 import json
+from typing import List
+
+import numpy as np
+
 from questions import Questions
 
 
 class Evaluation:
+    precision_recall_f1 = []
 
     def __init__(self, _query, _actual_ids):
         self.query = _query
@@ -32,8 +36,16 @@ class Evaluation:
         _precision = self.precision
         _recall = self.recall
         _f1 = self.f1(_precision, _recall)
+        Evaluation.precision_recall_f1.append((_precision, _recall, _f1))
         print("Query: {}".format(self.query))
         print("Precision: {}, Recall: {}, F1: {}".format(_precision, _recall, _f1))
+
+    @staticmethod
+    def mean_precision_recall_f1():
+        data_matrix = np.array(Evaluation.precision_recall_f1)
+        print("\nMean Precision: {}, Mean Recall: {}, Mean F1: {}\n".format(data_matrix[:, 0].mean(),
+                                                                            data_matrix[:, 1].mean(),
+                                                                            data_matrix[:, 2].mean()))
 
     @staticmethod
     def evaluate_json():
@@ -45,3 +57,4 @@ class Evaluation:
 
 if __name__ == "__main__":
     Evaluation.evaluate_json()
+    Evaluation.mean_precision_recall_f1()
